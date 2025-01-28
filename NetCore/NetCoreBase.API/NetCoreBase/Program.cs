9f8fecbb67d2457d3a578cdde8dfda55d7007032
Luis Espinoza <respinozabarboza@gmail.com>
2025-01-28 15:45:45 -0600

    add environment extension and mediatr extension, refator some code

--- NetCore/NetCoreBase.API/NetCoreBase/Program.cs
+++ NetCore/NetCoreBase.API/NetCoreBase/Program.cs
@@ -1,47 +1,24 @@
-
-using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using NetCoreBase.Application.Queries.GetByIdItemHandler;
-using NetCoreBase.Domain.Interfaces;
 using NetCoreBase.Extensions;
 using NetCoreBase.Infrastructure.Data.Postgresql;
-using System.Reflection;
-using static NetCoreBase.Application.Queries.GetByIdItemHandler.ItemGetByIdQuery;
-using NetCoreBase.Infrastructure.DataAccess;
 
 namespace NetCoreBase
 {
     public class Program
     {
-        //public IConfiguration _configuration { get; }
-
-        //public static IHostBuilder CreateHostBuilder(string[] args) =>
-        //Host.CreateDefaultBuilder(args)
-        //    .ConfigureAppConfiguration((hostingContext, config) =>
-        //    {
-        //        config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-        //        config.AddEnvironmentVariables();
-        //    })
-        //    .ConfigureServices((hostContext, services) =>
-        //    {
-        //        // Register your services here
-        //    });
-
         public static void Main(string[] args)
         {
-            //string myVariable = Environment.GetEnvironmentVariable("BASENETCORE_CONNECTION_STRING");
-
+            // Load environment-specific configuration
             var builder = WebApplication.CreateBuilder(args);
-            builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-
+            builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
             // Add services to the container.
-
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
             // add cors extension and Load CORS origins from appsettings.json            
             var allowedOrigins = builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>();
             if (allowedOrigins == null || allowedOrigins.Length == 0)
@@ -49,26 +26,32 @@ namespace NetCoreBase
                 throw new ArgumentException("AllowedOrigin configuration is missing or empty.");
             }
             builder.Services.AddCorsCustom("AllowSpecificOrigins", allowedOrigins);
-
-
-            //builder.Services.AddOptions();
             //register MediatR Handlers
-            //builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
-            builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(GetByIdItemHandler).GetTypeInfo().Assembly));
-
+            builder.Services.RegisterCommandsAndQueries();
             // register DI
             builder.Services.AddDependencyInjectionReferences();
-
+            // register database context
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
             options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"), b => b.MigrationsAssembly("NetCoreBase.Infrastructure")));
-
             //automapper
             builder.Services.AddAutoMapper(typeof(CustomMappingProfile).Assembly);
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
+            if (app.Environment.IsLocal())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+                app.UseDeveloperExceptionPage();
+            }
             if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+                app.UseDeveloperExceptionPage();
+            }
+            if (app.Environment.IsProduction())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
@@ -79,12 +62,9 @@ namespace NetCoreBase
 
             app.UseAuthorization();
 
-
             app.MapControllers();
 
             app.Run();
-        }
-
-        
+        }        
     }
 }
---
+++ NetCore/Readme.md
@@ -0,0 +1,18 @@
+
+
+
+# instructions
+
+Install sdk for local development or runtime for production environment
+
+## restore application packages
+
+```
+dotnet restore
+```
+
+## run appliction
+
+``` bash
+dotnet.exe run --project .\NetCoreBase.API\NetCoreBase\NetCoreBase.csproj --environment "Local"
+```
